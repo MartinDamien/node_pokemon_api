498ad4ad2b896536eba53c534da67a7a11b98194
DamDam <martindamien284@gmail.com>
2022-07-05 16:50:44 +0200

    premiere route

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/api/pokemon/1', (req, res) => {res.send(`hello, Bulbizarre`)});
+
 app.listen(port, () => console.log(`application lancé sur l'adresse : http://localhost:${port}, press Ctrl+C to stop, or Ctrl+Shift+C to quit.`));
