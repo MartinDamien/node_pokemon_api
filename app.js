fd6cc99f7fb1c2a36979e047e9e72cc261c6df84
DamDam <martindamien284@gmail.com>
2022-07-05 16:45:13 +0200

    nodemon

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`application lancé sur l'adresse : http://localhost:${port}, press Ctrl+C to stop, or Ctrl+Shift+C to quit.`));
