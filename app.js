26cdd5ddaf2467b2991832d656e2375abc73e160
DamDam <martindamien284@gmail.com>
2022-07-06 14:02:21 +0200

    commit on main

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+let pokemons = require("./mock_pockemon");
 
-app.get('/', (req, res) => res.send('Index Page'))
+const app = express();
+const port = 3000;
 
-app.get('/api/pokemon/:id', (req, res) => {
-    const id = req.params.id
-    res.send(`Vous avez demandez le pokemon n° ${id}`)
-    res.send(id)
-    })
+app.get("/", (req, res) => res.send("Index Page"));
 
-app.listen(port, () => console.log(`application lancé sur l'adresse : http://localhost:${port}, press Ctrl+C to stop, or Ctrl+Shift+C to quit.`))
+app.get("/api/pokemon/:id", (req, res) => {
+  // :id is a placeholder for the id of the pokemon
+  const id = parseInt(req.params.id); // parseInt convertie une chaine de caractere en un nombre
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id); // find returns the first element that satisfies the condition
+  res.json(pokemon);
+});
+
+app.get("/api/pokemon", (req, res) => {
+  res.send(`il y a ${pokemons.length} pokemons dans le pokedex pour le moment`);
+});
+
+app.listen(port, () =>
+  console.log(
+    `application lancé sur l'adresse : http://localhost:${port}, press Ctrl+C to stop, or Ctrl+Shift+C to quit.`
+  )
+);
